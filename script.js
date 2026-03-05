2f994013fd37f157e7835ce231adb2aa3a7ef06b
MonteroTom8Schwann <tommontero23@gmail.com>
2026-03-05 16:09:46 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function checkNumber() {
+  let n = document.getElementById("numberInput").value;
+
+  if (n === "") {
+    document.getElementById("result").innerText = "Please enter a number.";
+    return;
+  }
+
+  if (n % 2 == 0) {
+    document.getElementById("result").innerText = "EVEN";
+  } else {
+    document.getElementById("result").innerText = "ODD";
+  }
+}
